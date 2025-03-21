222173a6c3fbe92589f3ab60172cd8acda1d3ecb
Esperanza Fornes <fornes.borja@gmail.com>
2025-03-21 01:37:59 +0100

    feat:basic users table with no relations created

---
+++ src/main/java/com/fornesb/backend_ecommerce/entity/User.java
@@ -0,0 +1,98 @@
+package com.fornesb.backend_ecommerce.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "users")
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String role;
+    private String address;
+    private String phoneNumber;
+    private boolean isActive;
+
+    public User(Integer id, String name, String email, String password, String role, String address, String phoneNumber, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.isActive = isActive;
+    }
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
