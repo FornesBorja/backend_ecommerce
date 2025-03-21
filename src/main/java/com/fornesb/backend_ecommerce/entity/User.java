3345c8ff6350e4a00be872e081839f0927319899
Esperanza Fornes <fornes.borja@gmail.com>
2025-03-21 17:22:15 +0100

    feat: password related things added to users

--- src/main/java/com/fornesb/backend_ecommerce/entity/User.java
+++ src/main/java/com/fornesb/backend_ecommerce/entity/User.java
@@ -1,9 +1,15 @@
 package com.fornesb.backend_ecommerce.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
 @Table(name= "users")
+@Getter
+@Setter
 public class User
 {
     @Id
@@ -32,67 +38,19 @@ public class User
     public User() {
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
     public boolean isActive() {
         return isActive;
     }
 
+    public void setPassword(String rawPassword) {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        this.password = encoder.encode(rawPassword);
+    }
+
     public void setActive(boolean active) {
         isActive = active;
+    }
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.password = encryptedPassword;
     }
 }
