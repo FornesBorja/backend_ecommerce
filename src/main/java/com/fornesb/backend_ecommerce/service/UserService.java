bbe08319f03c6f3564b89766e41c23b6732343c1
Esperanza Fornes <fornes.borja@gmail.com>
2025-03-23 13:28:29 +0100

    feat:userService added

---
+++ src/main/java/com/fornesb/backend_ecommerce/service/UserService.java
@@ -0,0 +1,50 @@
+package com.fornesb.backend_ecommerce.service;
+
+import com.fornesb.backend_ecommerce.entity.User;
+import com.fornesb.backend_ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService
+{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Integer id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Integer id, User newUserData) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setName(newUserData.getName());
+                    user.setEmail(newUserData.getEmail());
+                    user.setAddress(newUserData.getAddress());
+                    user.setPhoneNumber(newUserData.getPhoneNumber());
+                    return userRepository.save(user);
+                }).orElse(null);
+    }
+
+    public boolean deleteUser(Integer id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
